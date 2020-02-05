395d1ba63db04f59d852ab7b9b5420a1eb6d1c42
sadie o'neill <sadie.r.oneill@gmail.com>
2020-02-05 14:57:41 -0700

    friends-with-pets

--- exercises/module3/friends-with-pets/src/App.js
+++ exercises/module3/friends-with-pets/src/App.js
@@ -3,12 +3,13 @@ import friendsArr from "./FriendList.js"
 import Friend from "./Friend.js"
 
 function App(){
-    const animals = friendsArr.map(pet => <Friend myLittleFriend ={pet}/>)
+    const animals = friendsArr.map(pet => 
+    <Friend myLittleFriend ={pet}/>)
     
     return (
-        <div>
+        <>
             {animals}
-        </div>
+        </>
     )
 }
 
