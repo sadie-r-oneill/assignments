c17f9ff1bc3afb7412c209cfe16286ae8c6a8812
sadie o'neill <sadie.r.oneill@gmail.com>
2020-03-01 13:48:38 -0700

    portfolio

--- exercises/module-4/portfolio/src/Email.js
+++ exercises/module-4/portfolio/src/Email.js
@@ -3,11 +3,17 @@ import React from "react"
 function Email(){
     return(
         <div>
+            <h1 className = "developer">Web Developer based out of Salt Lake City, UT</h1>
             <h1 className = "myEmail">Email: sadie.r.oneill@gmail.com</h1>
             <h1 className = "phone">Phone: 818-438-9074</h1>
-            <p className ="bio">
+            <h1 className = "language">Languages I use:</h1>
+            <ul className = "bio">
+                <li>JavaScript</li>
+                <li>React, Redux, Git, CSS</li>
+                <li> MongoDB, Node.js, HTML</li>
+                <li>JSON Web Tokens, Express</li>
                 
-            </p>
+            </ul>
         </div>
     )
 }
